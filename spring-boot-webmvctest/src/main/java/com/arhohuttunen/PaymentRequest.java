7a96e99f2cfa12c626df3de4b625015c75496082
Arho Huttunen <arho.huttunen@iki.fi>
2021-04-25 08:51:22 +0300

    Add unit separate unit tests for constraint violation verification

--- spring-boot-webmvctest/src/main/java/com/arhohuttunen/PaymentRequest.java
+++ spring-boot-webmvctest/src/main/java/com/arhohuttunen/PaymentRequest.java
@@ -1,13 +1,20 @@
 package com.arhohuttunen;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.CreditCardNumber;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class PaymentRequest {
     @NotNull
+    @CreditCardNumber
     private String creditCardNumber;
 }
