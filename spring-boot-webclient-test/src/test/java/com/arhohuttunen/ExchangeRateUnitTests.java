176778ba3157b0d153372bd15cdc6bbdcc75d729
Arho Huttunen <arho.huttunen@iki.fi>
2021-05-23 10:11:10 +0300

    Add examples of bad unit tests

---
+++ spring-boot-webclient-test/src/test/java/com/arhohuttunen/ExchangeRateUnitTests.java
@@ -0,0 +1,56 @@
+package com.arhohuttunen;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import javax.money.CurrencyUnit;
+import javax.money.Monetary;
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+// This is an example of a bad unit test and should not be taken as an advice on how to test WebClient
+
+public class ExchangeRateUnitTests {
+    private WebClient.RequestHeadersUriSpec requestHeadersUriMock;
+    private WebClient.RequestBodySpec requestBodyMock;
+    private WebClient.ResponseSpec responseMock;
+    private WebClient webClientMock;
+
+    @BeforeEach
+    void mockWebClient() {
+        requestHeadersUriMock = mock(WebClient.RequestHeadersUriSpec.class);
+        requestBodyMock = mock(WebClient.RequestBodySpec.class);
+        responseMock = mock(WebClient.ResponseSpec.class);
+        webClientMock = mock(WebClient.class);
+    }
+
+    @Test
+    void exchangeCurrency() {
+        ExchangeClientProperties properties = new ExchangeClientProperties();
+        properties.setApiKey("apiKey");
+        properties.setBaseUrl("http://localhost");
+
+        ExchangeRateClient client = new ExchangeRateClient(webClientMock, properties);
+
+        ExchangeResponse response = new ExchangeResponse();
+        response.setConversionRate(BigDecimal.valueOf(0.748));
+
+        CurrencyUnit eur = Monetary.getCurrency("EUR");
+        CurrencyUnit usd = Monetary.getCurrency("USD");
+
+        String expectedUri = "http://localhost/v6/{apiKey}/pair/{from}/{to}";
+        when(webClientMock.get()).thenReturn(requestHeadersUriMock);
+        when(requestHeadersUriMock.uri(expectedUri, "apiKey", eur, usd)).thenReturn(requestBodyMock);
+        when(requestBodyMock.retrieve()).thenReturn(responseMock);
+        when(responseMock.bodyToMono(ExchangeResponse.class)).thenReturn(Mono.just(response));
+
+        BigDecimal rate = client.getExchangeRate(eur, usd);
+
+        assertThat(rate.doubleValue()).isEqualTo(0.748);
+    }
+}
---
+++ spring-boot-webclient-test/src/test/java/com/arhohuttunen/TwilioClientUnitTests.java
@@ -0,0 +1,51 @@
+package com.arhohuttunen;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+// This is an example of a bad unit test and should not be taken as an advice on how to test WebClient
+
+public class TwilioClientUnitTests {
+    private WebClient.RequestBodyUriSpec requestBodyUriMock;
+    private WebClient.RequestHeadersSpec requestHeadersMock;
+    private WebClient.RequestBodySpec requestBodyMock;
+    private WebClient.ResponseSpec responseMock;
+    private WebClient webClientMock;
+
+    @BeforeEach
+    void mockWebClient() {
+        requestBodyUriMock = mock(WebClient.RequestBodyUriSpec.class);
+        requestHeadersMock = mock(WebClient.RequestHeadersSpec.class);
+        requestBodyMock = mock(WebClient.RequestBodySpec.class);
+        responseMock = mock(WebClient.ResponseSpec.class);
+        webClientMock = mock(WebClient.class);
+    }
+
+    @Test
+    void sendSms() {
+        TwilioClientProperties properties = new TwilioClientProperties();
+        properties.setBaseUrl("http://localhost");
+        properties.setAccountSid("accountSid");
+
+        TwilioClient client = new TwilioClient(webClientMock, properties);
+
+        TwilioMessageResponse response = new TwilioMessageResponse();
+        TwilioMessageRequest request = new TwilioMessageRequest("5678", "1234", "message");
+        String expectedUri = "http://localhost/Accounts/{AccountSid}/Messages.json";
+
+        when(webClientMock.post()).thenReturn(requestBodyUriMock);
+        when(requestBodyUriMock.uri(eq(expectedUri), eq("accountSid"))).thenReturn(requestBodyMock);
+        when(requestBodyMock.bodyValue(eq(request))).thenReturn(requestHeadersMock);
+        when(requestHeadersMock.retrieve()).thenReturn(responseMock);
+        when(responseMock.bodyToMono(TwilioMessageResponse.class)).thenReturn(Mono.just(response));
+
+        assertDoesNotThrow(() -> client.sendSms("1234", "5678", "message"));
+    }
+}
