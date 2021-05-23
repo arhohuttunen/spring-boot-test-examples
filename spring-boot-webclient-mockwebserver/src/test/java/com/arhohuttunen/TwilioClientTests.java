913147a72973b8c3e7e622308edca6e5f6c85748
Arho Huttunen <arho.huttunen@iki.fi>
2021-05-23 10:17:37 +0300

    Decided to leave the helper methods out from the article for simplicity

--- spring-boot-webclient-mockwebserver/src/test/java/com/arhohuttunen/TwilioClientTests.java
+++ spring-boot-webclient-mockwebserver/src/test/java/com/arhohuttunen/TwilioClientTests.java
@@ -13,7 +13,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.function.Consumer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,63 +33,37 @@ public class TwilioClientTests {
     }
 
     @Test
-    void makesTheCorrectRequest() {
-        prepareResponse(response ->
-                response.setResponseCode(200)
+    void makesTheCorrectRequest() throws InterruptedException {
+        mockWebServer.enqueue(
+                new MockResponse().setResponseCode(200)
                         .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                         .setBody(getJson("message-response.json"))
         );
 
         twilioClient.sendSms("+123456", "+234567", "test message");
 
-        expectRequest(request -> {
-            assertThat(request.getMethod()).isEqualTo("POST");
-            assertThat(request.getPath()).isEqualTo("/Accounts/ACd936ed6dc1504dd79530f19f57b9c008/Messages.json");
-        });
+        RecordedRequest request = mockWebServer.takeRequest();
+
+        assertThat(request.getMethod()).isEqualTo("POST");
+        assertThat(request.getPath()).isEqualTo("/Accounts/ACd936ed6dc1504dd79530f19f57b9c008/Messages.json");
     }
 
     @Test
-    void serializesRequest() {
-        prepareResponse(response ->
-                response.setResponseCode(200)
+    void serializesRequest() throws InterruptedException {
+        mockWebServer.enqueue(
+                new MockResponse().setResponseCode(200)
                         .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                         .setBody(getJson("message-response.json"))
         );
 
         twilioClient.sendSms("+123456", "+234567", "test message");
 
-        expectRequestBody(body -> {
-            assertThat(body).extractingJsonPathStringValue("$.from").isEqualTo("+123456");
-            assertThat(body).extractingJsonPathStringValue("$.to").isEqualTo("+234567");
-            assertThat(body).extractingJsonPathStringValue("$.body").isEqualTo("test message");
+        RecordedRequest request = mockWebServer.takeRequest();
+        JsonContent<Object> body = json.from(request.getBody().readUtf8());
 
-        });
-    }
-
-    private void prepareResponse(Consumer<MockResponse> consumer) {
-        MockResponse response = new MockResponse();
-        consumer.accept(response);
-        this.mockWebServer.enqueue(response);
-    }
-
-    private void expectRequest(Consumer<RecordedRequest> consumer) {
-        try {
-            consumer.accept(this.mockWebServer.takeRequest());
-        }
-        catch (InterruptedException ex) {
-            throw new IllegalStateException(ex);
-        }
-    }
-
-    private void expectRequestBody(Consumer<JsonContent<Object>> consumer) {
-        try {
-            RecordedRequest request = mockWebServer.takeRequest();
-            JsonContent<Object> requestBody = json.from(request.getBody().readUtf8());
-            consumer.accept(requestBody);
-        }
-        catch (InterruptedException ex) {
-            throw new IllegalStateException(ex);
-        }
+        assertThat(body).extractingJsonPathStringValue("$.from").isEqualTo("+123456");
+        assertThat(body).extractingJsonPathStringValue("$.to").isEqualTo("+234567");
+        assertThat(body).extractingJsonPathStringValue("$.body").isEqualTo("test message");
     }
 
     private String getJson(String path) {
